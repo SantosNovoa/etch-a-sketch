2fe03019449ee789e5f0e9c840881f1a3223c93a
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-07 23:36:11 -0400

    Added a color feature
    User can now color the grid by holding the mouse down. Individual cells well be colored black.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const screen = document.querySelector(".screen");
+const cell = document.querySelector("cell");
 
-console.log(screen);
+
+
+
+
 
 function createGrid(x) {
   const newWidth = 660 / x;
@@ -15,10 +19,21 @@ function createGrid(x) {
       cell.style.width = `${newWidth}px`;
       cell.style.height = `${newHeight}px`;
       row.appendChild(cell);
+
+
+      const cells = Array.from(document.getElementsByClassName('cell'));
+      let isDrawing = false; 
+      window.onmouseup = () => { isDrawing = false; }
+      cells.forEach(cell => {
+          cell.onmouseover = () => { if(isDrawing) cell.style.backgroundColor="black"; }
+          cell.onmousedown = () => { cell.style.backgroundColor="black"; isDrawing = true; }                  
+      });
     }
+
+    
     screen.appendChild(row);
   }
 }
 
-// Example: Create a 5x5 table
-createGrid(16);
+// Create a 16x16 table
+createGrid(50);
