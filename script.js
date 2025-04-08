1cfff762f8860195e4763467dc709a65a6fb74eb
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-08 11:35:21 -0400

    Added a function to accept user input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const screen = document.querySelector(".screen");
 const cell = document.querySelector("cell");
-
-
+const inputElement = document.getElementById("quantity");
+const inputValue = inputElement.value;
+const form = document.getElementById("my_form");
+form.addEventListener('submit', submitForm);
 
 
 
@@ -9,6 +11,7 @@ const cell = document.querySelector("cell");
 function createGrid(x) {
   const newWidth = 660 / x;
   const newHeight = 660 / x;
+
   for (let i = 0; i < x; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -19,21 +22,31 @@ function createGrid(x) {
       cell.style.width = `${newWidth}px`;
       cell.style.height = `${newHeight}px`;
       row.appendChild(cell);
-
-
-      const cells = Array.from(document.getElementsByClassName('cell'));
+    }
+    screen.appendChild(row);
+  }
+  const cells = Array.from(document.getElementsByClassName('cell'));
       let isDrawing = false; 
       window.onmouseup = () => { isDrawing = false; }
       cells.forEach(cell => {
           cell.onmouseover = () => { if(isDrawing) cell.style.backgroundColor="black"; }
           cell.onmousedown = () => { cell.style.backgroundColor="black"; isDrawing = true; }                  
       });
-    }
-
-    
-    screen.appendChild(row);
-  }
 }
 
+
+
+function clearGrid() {
+  cell.remove();
+}
+
+console.log(16)
 // Create a 16x16 table
-createGrid(50);
+function submitForm(e) {
+  e.preventDefault(); // stop page from reloading
+  screen.innerHTML = "";
+  const size = Number(inputElement.value);
+  createGrid(size);
+}
+
+createGrid(16)
